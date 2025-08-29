5a8b7e5c911f420da2b243a4db5d0cb96d2aa7bc
Benjamin Melendez <melendezcastilloemer@gmail.com>
2025-08-29 14:00:29 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="App-GitHub",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Benjamin Melendez",                         # Tu nombre
+    author_email="melendezcastilloemer@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Benwhite7/App-GitHub",     # URL del proyecto
+)
